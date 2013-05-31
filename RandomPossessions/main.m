a700c31f7457751e882540b41a676c829fc4c96d
Tim Payton <timpayton@gmail.com>
2013-05-31 22:29:38 +0200

    Initial Commit

---
+++ RandomPossessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPossessions
+//
+//  Created by tim_payton on 5/31/13.
+//  Copyright (c) 2013 tim_payton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
